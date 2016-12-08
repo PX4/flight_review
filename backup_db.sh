b27a0d289d89be438d42651b80d22e31b0841bbd
Beat Küng <beat-kueng@gmx.net>
2016-12-08 14:17:48 +0100

    backup_db.sh: make a copy of the .sqlite db in addition to the .sql dump

--- backup_db.sh
+++ backup_db.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 db_file="data/logs.sqlite"
-backup_file="backups/backup_db_`date +%Y-%m-%d`.sql"
+backup_file="backups/backup_db_`date +%Y-%m-%d`"
 [ ! -d backups ] && mkdir backups
 
-sqlite3 $db_file "select * from Logs" >$backup_file
-echo "Backed up `wc -l $backup_file` records from DB"
+cp $db_file ${backup_file}.sqlite
+sqlite3 $db_file "select * from Logs" >${backup_file}.sql
+echo "Backed up `wc -l ${backup_file}.sql` records from DB"
