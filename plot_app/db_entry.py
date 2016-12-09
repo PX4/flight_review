c0818c906c31b495c62d8cb0592961405c2268c4
Beat Küng <beat-kueng@gmx.net>
2016-12-09 09:18:36 +0100

    DB: add & use a LogsGenerated table for faster retrieval of the browse page

--- plot_app/db_entry.py
+++ plot_app/db_entry.py
@@ -31,34 +31,50 @@ class DBData:
 class DBDataGenerated:
     """ information from the generated DB entry """
 
-    def __init__(self, log_id):
+    def __init__(self):
+        self.duration_s = 0
+        self.mav_type = ''
+        self.estimator = ''
+        self.sys_autostart_id = 0
+        self.sys_hw = ''
+        self.ver_sw = ''
+        self.num_logged_errors = 0
+        self.num_logged_warnings = 0
+        self.flight_modes = set()
+
+
+    @classmethod
+    def fromLogFile(cls, log_id):
         """ initialize from a log file """
+        obj = cls()
 
         ulog_file_name = get_log_filename(log_id)
         ulog = ULog(ulog_file_name)
         px4_ulog = PX4ULog(ulog)
 
         # extract information
-        self.duration_s = int((ulog.last_timestamp - ulog.start_timestamp)/1e6)
-        self.mav_type = px4_ulog.get_mav_type()
-        self.estimator = px4_ulog.get_estimator()
-        self.sys_autostart_id = ulog.initial_parameters.get('SYS_AUTOSTART', 0)
-        self.sys_hw = cgi.escape(ulog.msg_info_dict.get('ver_hw', ''))
-        self.ver_sw = cgi.escape(ulog.msg_info_dict.get('ver_sw', ''))
-        self.num_logged_errors = 0
-        self.num_logged_warnings = 0
+        obj.duration_s = int((ulog.last_timestamp - ulog.start_timestamp)/1e6)
+        obj.mav_type = px4_ulog.get_mav_type()
+        obj.estimator = px4_ulog.get_estimator()
+        obj.sys_autostart_id = ulog.initial_parameters.get('SYS_AUTOSTART', 0)
+        obj.sys_hw = cgi.escape(ulog.msg_info_dict.get('ver_hw', ''))
+        obj.ver_sw = cgi.escape(ulog.msg_info_dict.get('ver_sw', ''))
+        obj.num_logged_errors = 0
+        obj.num_logged_warnings = 0
 
         for m in ulog.logged_messages:
             if m.log_level <= ord('3'):
-                self.num_logged_errors += 1
+                obj.num_logged_errors += 1
             if m.log_level == ord('4'):
-                self.num_logged_warnings += 1
+                obj.num_logged_warnings += 1
 
         try:
             cur_dataset = [ elem for elem in ulog.data_list
                     if elem.name == 'commander_state' and elem.multi_id == 0][0]
             flight_mode_changes = cur_dataset.list_value_changes('main_state')
-            self.flight_modes = set([x[1] for x in flight_mode_changes])
+            obj.flight_modes = set([x[1] for x in flight_mode_changes])
         except (KeyError,IndexError) as error:
-            self.flight_modes = set()
+            obj.flight_modes = set()
+
+        return obj
 
