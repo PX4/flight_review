0159d934b52e1bc82c59121c6a139f0386fb2672
Beat Küng <beat-kueng@gmx.net>
2016-12-22 18:32:52 +0100

    add prune_old_logs.py: script to remove old (CI) log files & DB entries

---
+++ prune_old_logs.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+# Script to delete old log files & DB entries matching a certain criteria
+
+import sqlite3
+import sys
+import os
+import argparse
+import datetime
+
+# this is needed for the following imports
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'plot_app'))
+from plot_app.config import get_db_filename
+from plot_app.helper import get_log_filename
+
+
+parser = argparse.ArgumentParser(description='Remove a DB entry (but not the log file)')
+
+parser.add_argument('--max-age', action='store', type=int, default=30,
+        help='maximum age in days (delete logs older than this, default=30)')
+parser.add_argument('--source', action='store', default='CI',
+        help='Source DB entry tag to match (empty=all, default=CI)')
+parser.add_argument('--interactive', '-i', action='store_true', default=False,
+        help='Interative mode: ask whether to delete the entries')
+
+args = parser.parse_args()
+
+
+max_age = args.max_age
+source = args.source
+interactive = args.interactive
+
+
+
+con = sqlite3.connect(get_db_filename(), detect_types=sqlite3.PARSE_DECLTYPES)
+with con:
+    cur = con.cursor()
+    log_ids_to_remove = []
+
+    if len(source) == 0:
+        cur.execute('select Id, Date, Description from Logs')
+    else:
+        cur.execute('select Id, Date, Description from Logs where Source = ?', [source])
+
+    db_tuples = cur.fetchall()
+    print('will delete the following:')
+    for db_tuple in db_tuples:
+        log_id = db_tuple[0]
+        date = db_tuple[1]
+        description = db_tuple[2]
+
+        # check date
+        elapsed_days = (datetime.datetime.now()-date).days
+        if elapsed_days > max_age:
+            print('{} {} {}'.format(log_id, date.strftime('%Y_%m_%d-%H_%M'),
+                description))
+            log_ids_to_remove.append(log_id)
+
+
+    if len(log_ids_to_remove) == 0:
+        print('no maches. exiting')
+        exit(0)
+
+    if interactive:
+        confirm = input('Press "y" and ENTER to confirm and delete: ')
+        if confirm != 'y':
+            print('Not deleting anything')
+            exit(0)
+
+    for log_id in log_ids_to_remove:
+        print('Removing '+log_id)
+        # db entry
+        cur.execute("DELETE FROM LogsGenerated WHERE Id = ?", (log_id,))
+        cur.execute("DELETE FROM Logs WHERE Id = ?", (log_id,))
+        num_deleted = cur.rowcount
+        if num_deleted != 1:
+            print('Error: not found ({})'.format(num_deleted))
+        con.commit()
+
+        # and the log file
+        ulog_file_name = get_log_filename(log_id)
+        os.unlink(ulog_file_name)
+
+con.close()
+
