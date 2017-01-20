2d02a0089d639c48ca4cecb050fee0b392ec87e2
Beat Küng <beat-kueng@gmx.net>
2017-01-20 10:11:41 +0100

    colors.py: pylint fixes

--- plot_app/colors.py
+++ plot_app/colors.py
@@ -1,13 +1,16 @@
+""" color helper functions """
 
+#pylint: disable=invalid-name
 
 import colorsys
 
 def get_N_colors(N, s=0.8, v=0.9):
+    """ get N distinct colors as a list of hex strings """
     HSV_tuples = [(x*1.0/N, s, v) for x in range(N)]
     hex_out = []
     for rgb in HSV_tuples:
-        rgb = map(lambda x: int(x*255),colorsys.hsv_to_rgb(*rgb))
-        hex_out.append("#"+"".join(map(lambda x: format(x, '02x'),rgb)))
+        rgb = map(lambda x: int(x*255), colorsys.hsv_to_rgb(*rgb))
+        hex_out.append("#"+"".join(map(lambda x: format(x, '02x'), rgb)))
     return hex_out
 
 def HTML_color_to_RGB(html_color):
