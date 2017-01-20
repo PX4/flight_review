209b3bc3530b55fb0531b0ea2e261d718941512e
Beat Küng <beat-kueng@gmx.net>
2017-01-20 10:13:26 +0100

    send_email.py: pylint fixes

--- send_email.py
+++ send_email.py
@@ -1,28 +1,31 @@
+""" Methods for sending notification emails """
+from __future__ import print_function
 
 import sys
 import os
+
+from smtplib import SMTP_SSL as SMTP       # this invokes the secure SMTP protocol
+                                           # (port 465, uses SSL)
+# from smtplib import SMTP                  # use this for standard SMTP protocol
+                                           # (port 25, no encryption)
+from email.mime.text import MIMEText
 
 # this is needed for the following imports
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'plot_app'))
 from plot_app.config import *
 
-from smtplib import SMTP_SSL as SMTP       # this invokes the secure SMTP protocol (port 465, uses SSL)
-# from smtplib import SMTP                  # use this for standard SMTP protocol   (port 25, no encryption)
-from email.mime.text import MIMEText
-
-
 
 def send_notification_email(email_adress, plot_url, log_description,
-        delete_url):
+                            delete_url):
     """ send a notification email after uploading a plot """
 
     if email_adress == '':
         return True
 
-    subject="Log File uploaded"
+    subject = "Log File uploaded"
     destination = [email_adress]
 
-    content="""\
+    content = """\
 Hi there!
 
 Your uploaded log file with description '{description}' is available under:
@@ -31,19 +34,19 @@ Your uploaded log file with description '{description}' is available under:
 Use the following link to delete the log:
 {delete_url}
 """.format(plot_url=plot_url, description=log_description,
-        delete_url=delete_url)
+           delete_url=delete_url)
 
     return _send_email(destination, subject, content)
 
 
 def send_flightreport_email(destination, plot_url, log_description, feedback,
-        rating, wind_speed, delete_url, uploader_email):
+                            rating, wind_speed, delete_url, uploader_email):
     """ send notification email for a flight report upload """
 
     if len(destination) == 0:
         return True
 
-    content="""\
+    content = """\
 Hi
 
 A new flight report just got uploaded:
@@ -58,10 +61,10 @@ Uploader: {uploader_email}
 Use the following link to delete the log:
 {delete_url}
 """.format(plot_url=plot_url, description=log_description,
-    feedback=feedback, rating=rating, wind_speed=wind_speed,
-    delete_url=delete_url, uploader_email=uploader_email)
+           feedback=feedback, rating=rating, wind_speed=wind_speed,
+           delete_url=delete_url, uploader_email=uploader_email)
 
-    subject="Flight Report uploaded"
+    subject = "Flight Report uploaded"
 
     return _send_email(destination, subject, content)
 
