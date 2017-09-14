9e0ad12a03bb2e13a74a6fe5a35d7317a0d80905
Anass Al <mr@anassinator.com>
2017-09-14 00:35:31 +0000

    download_logs: fix syntax error

--- download_logs.py
+++ download_logs.py
@@ -4,6 +4,7 @@ import os, glob
 import argparse
 import requests
 
+
 def get_arguments():
     parser = argparse.ArgumentParser(description='Python script for downloading public logs from the PX4/flight_review database.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -11,7 +12,7 @@ def get_arguments():
                         help='Maximum number of files to download that match the search criteria. set to -1 to download all files.')
     parser.add_argument('-d', '--download-folder', type=str, default="data/downloaded/",
                         help='The folder to store the downloaded logfiles.')
-    parser.add_argument('--print', action='store_true',
+    parser.add_argument('--print', action='store_true', dest="print_entries",
                         help='Whether to only print (not download) the database entries.')
     parser.add_argument('--overwrite', action='store_true', default=False,
                         help='Whether to overwrite already existing files in download folder.')
@@ -21,19 +22,18 @@ def get_arguments():
                         help='The url at which the server provides the download API.')
     return parser.parse_args()
 
-def main():
 
+def main():
     args = get_arguments()
 
     try:
         # the db_info_api sends a json file with a list of all public database entries
         db_entries_list = requests.get(url=args.db_info_api).json()
-
     except:
         print("Server request failed.")
         raise
 
-    if args.print:
+    if args.print_entries:
         # only print the json output
         print(db_entries_list)
     else:
@@ -69,6 +69,7 @@ def main():
 
         print('{:} logs downloaded to {:}, {:} logs skipped (already downloaded)'.format(
             n_downloaded, args.download_folder, n_skipped))
+
 
 if __name__ == '__main__':
     main()
