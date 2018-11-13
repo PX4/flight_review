3b22ca26222f588a760c880b3778aa15b7cde929
Ramón Hernán Roche Quintana <mrpollo@gmail.com>
2018-11-13 10:37:50 -0800

    python: requirements.txt file and update strategy

---
+++ run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# Check requirements lock file
+pipenv lock --requirements > local-requirements.txt
+cmp --silent requirements.txt local-requirements.txt || echo "ERROR: Pipenv.lock difers from requirements.txt" && exit 1
+
+# Check pylint
+# TODO: fix pylint on source and enable
+#bash run_pylint.sh
