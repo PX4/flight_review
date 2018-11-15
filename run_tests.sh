a9344f8ae7ea72b3e879f66812562c5c1ff3bc11
Ramón Hernán Roche Quintana <mrpollo@gmail.com>
2018-11-15 10:23:33 -0800

    ci: enable pylint add helpful messages

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 set -e
+echo "[$(date)] Starting tests."
 
+echo "[$(date)] Validating python dependency match"
 # Check requirements lock file
 pipenv lock --requirements > local-requirements.txt
-cmp --silent requirements.txt local-requirements.txt || echo "ERROR: Pipenv.lock difers from requirements.txt"
+cmp --silent requirements.txt local-requirements.txt || echo "[$(date)] ERROR: Pipenv.lock difers from requirements.txt"
 
-# Check pylint
-# TODO: fix pylint on source and enable
-#bash run_pylint.sh
+echo "[$(date)] Running pylint"
+bash run_pylint.sh
+
+echo "[$(date)] End of test script."
