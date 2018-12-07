25e5c144282b78e6655c3435e2b86e8b6952366d
Vit Hanousek <vithanousek@seznam.cz>
2018-12-07 12:22:55 +0100

    Cleaning code.

--- plot_app/overview_generator.py
+++ plot_app/overview_generator.py
@@ -1,55 +1,51 @@
-import os 
+"""
+Module for generating overview map
+"""
+
+import os
 
 import smopy
-from IPython.display import Image
-
 import matplotlib.pyplot as plt
 
 def generate_overview_img(ulog, log_id, output_path):
-   ''' This map overview image is loaded by browse page
+    ''' This map overview image is loaded by browse page
 
-   '''
+    '''
 
-   output_filename=os.path.join(output_path,log_id+'.png')
+    output_filename = os.path.join(output_path, log_id+'.png')
 
-   print(output_filename)
- 
-   if os.path.exists(output_filename):
-      return
+    print(output_filename)
 
-   try:
-      cur_dataset = ulog.get_dataset('vehicle_gps_position')
-      t = cur_dataset.data['timestamp']
-      indices = cur_dataset.data['fix_type'] > 2 # use only data with a fix
-      #t = t[indices]
-      lon = cur_dataset.data['lon'][indices] / 1e7 # degrees
-      lat = cur_dataset.data['lat'][indices] / 1e7
-      #altitude = cur_dataset.data['alt'][indices] / 1e3 # meters
+    if os.path.exists(output_filename):
+        return
 
-      min_lat=min(lat);
-      max_lat=max(lat);
+    try:
+        cur_dataset = ulog.get_dataset('vehicle_gps_position')
+        t = cur_dataset.data['timestamp']
+        indices = cur_dataset.data['fix_type'] > 2 # use only data with a fix
+        lon = cur_dataset.data['lon'][indices] / 1e7 # degrees
+        lat = cur_dataset.data['lat'][indices] / 1e7
 
-      min_lon=min(lon);
-      max_lon=max(lon);
-     
+        min_lat = min(lat)
+        max_lat = max(lat)
 
-      map = smopy.Map((min_lat,min_lon,max_lat,max_lon))
-      fig, ax = plt.subplots( nrows=1, ncols=1 ) 
-      map.show_mpl(figsize=(8, 6), ax=ax)
+        min_lon = min(lon)
+        max_lon = max(lon)
 
-      for i in range(len(lat)):
-         x, y = map.to_pixels(lat[i], lon[i])
-         ax.plot(x, y,'.r');
+        render_map = smopy.Map((min_lat, min_lon, max_lat, max_lon))
+        fig, axes = plt.subplots(nrows=1, ncols=1)
+        render_map.show_mpl(figsize=(8, 6), ax=axes)
 
-      ax.set_axis_off()
-      plt.savefig(output_filename, bbox_inches='tight')
-      plt.close(fig)
+        for i in range(len(lat)):
+            x, y = render_map.to_pixels(lat[i], lon[i])
+            axes.plot(x, y, '.r')
 
-      print('Saving overview file '+ output_filename)
-   except:
-      # Ignore. Eg. if topic not found
-      print('Error generating overview file: '+ output_filename+' - No GPS?')
-      pass
+        axes.set_axis_off()
+        plt.savefig(output_filename, bbox_inches='tight')
+        plt.close(fig)
 
-
+        print('Saving overview file '+ output_filename)
+    except:
+        # Ignore. Eg. if topic not found
+        print('Error generating overview file: '+ output_filename+' - No GPS?')
 
