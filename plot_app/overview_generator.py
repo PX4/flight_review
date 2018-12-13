eb12b003f9f81505bb7f7e62671d930d71fd1802
Vit Hanousek <vithanousek@seznam.cz>
2018-12-13 12:48:14 +0100

    Map_overview: speed up of generation 	-> reduce Map Zoom 	-> do not render line in for loop (I can use Map.to_pixel for arrays)

--- plot_app/overview_generator.py
+++ plot_app/overview_generator.py
@@ -3,14 +3,24 @@ Module for generating overview map
 """
 
 import os
-
 import smopy
 import matplotlib.pyplot as plt
+
+#we overide of this function from Map to get lover zoom
+MAXTILES=16
+def get_zoom(input_box, z=18):
+    box = smopy._box(input_box )
+    box_tile = smopy.get_tile_box(box, z)
+    box = smopy.correct_box(box_tile, z)
+    sx, sy = smopy.get_box_size(box)
+    if sx * sy >= MAXTILES:
+        z = get_zoom(input_box, z - 1)
+    return z
 
 def generate_overview_img(ulog, log_id, output_path):
     ''' This map overview image is loaded by browse page
 
-    '''
+        '''
 
     output_filename = os.path.join(output_path, log_id+'.png')
 
@@ -30,19 +40,23 @@ def generate_overview_img(ulog, log_id, output_path):
         min_lon = min(lon)
         max_lon = max(lon)
 
-        render_map = smopy.Map((min_lat, min_lon, max_lat, max_lon))
+        z = get_zoom((min_lat, min_lon, max_lat, max_lon)) - 2
+        if z < 0:
+            z = 0
+
+        render_map = smopy.Map((min_lat, min_lon, max_lat, max_lon), z=z)    
         fig, axes = plt.subplots(nrows=1, ncols=1)
         render_map.show_mpl(figsize=(8, 6), ax=axes)
 
-        for i in range(len(lat)):
-            x, y = render_map.to_pixels(lat[i], lon[i])
-            axes.plot(x, y, '.r')
+        x, y = render_map.to_pixels(lat, lon)
+        axes.plot(x, y, 'r')
 
         axes.set_axis_off()
         plt.savefig(output_filename, bbox_inches='tight')
         plt.close(fig)
 
         print('Saving overview file '+ output_filename)
+
     except:
         # Ignore. Eg. if topic not found
         print('Error generating overview file: '+ output_filename+' - No GPS?')
