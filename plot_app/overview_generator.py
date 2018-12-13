9e2ac56529943c781538b21f7c62f577ee07456e
Vit Hanousek <vithanousek@seznam.cz>
2018-12-13 13:28:21 +0100

    Code cleaning

--- plot_app/overview_generator.py
+++ plot_app/overview_generator.py
@@ -6,11 +6,12 @@ import os
 import smopy
 import matplotlib.pyplot as plt
 
-#we overide of this function from Map to get lover zoom
-MAXTILES=16
+MAXTILES = 16
 def get_zoom(input_box, z=18):
-    box = smopy._box(input_box )
-    box_tile = smopy.get_tile_box(box, z)
+    """
+    Return acceptable zoom - we take this function from Map to get lover zoom
+    """
+    box_tile = smopy.get_tile_box(input_box, z)
     box = smopy.correct_box(box_tile, z)
     sx, sy = smopy.get_box_size(box)
     if sx * sy >= MAXTILES:
@@ -44,7 +45,7 @@ def generate_overview_img(ulog, log_id, output_path):
         if z < 0:
             z = 0
 
-        render_map = smopy.Map((min_lat, min_lon, max_lat, max_lon), z=z)    
+        render_map = smopy.Map((min_lat, min_lon, max_lat, max_lon), z=z)
         fig, axes = plt.subplots(nrows=1, ncols=1)
         render_map.show_mpl(figsize=(8, 6), ax=axes)
 
