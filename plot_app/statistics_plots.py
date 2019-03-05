6dac0f6f724066b11c5d49701b74843b9f86091f
johannes <johannes@auterion.com>
2019-03-05 10:40:26 +0100

    remove ci logs from statistics plots

--- plot_app/statistics_plots.py
+++ plot_app/statistics_plots.py
@@ -129,7 +129,10 @@ class StatisticsPlots:
 
                 self._all_logs_dates.append(log.date)
                 if log.is_public == 1:
-                    self._public_logs_dates.append(log.date)
+                    if log.source == 'CI':
+                        self._ci_logs_dates.append(log.date)
+                    else:
+                        self._public_logs_dates.append(log.date)
                 else:
                     if log.source == 'CI':
                         self._ci_logs_dates.append(log.date)
@@ -138,7 +141,7 @@ class StatisticsPlots:
 
 
                 # LogsGenerated: public only
-                if log.is_public != 1:
+                if log.is_public != 1 or log.source == 'CI':
                     continue
 
                 cur.execute('select * from LogsGenerated where Id = ?', [log.log_id])
