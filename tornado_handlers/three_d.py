491635951436bcf6ea17cc2ae3b3c7abb2c83694
sozel@wpi.edu <selim@Skynet.local>
2019-04-15 12:49:26 -0700

    Added error detection to python side and javascript side. If optional data streams are not found default behaviour is to not plot it on the 2D streamer.

--- tornado_handlers/three_d.py
+++ tornado_handlers/three_d.py
@@ -48,22 +48,67 @@ class ThreeDHandler(TornadoRequestHandlerBase):
                 '(vehicle_gps_position, vehicle_global_position, '
                 'vehicle_attitude)')
 
-        # manual control setpoint is optional
+        # These are optional data streams. Most of them are added
+        # for charting/streaming 2D plots on the 3D viewer.
         manual_control_setpoint = None
         vehicle_local_position = None
         vehicle_local_position_setpoint = None
+        vehicle_attitude_setpoint = None
+        vehicle_rates_setpoint = None
+        actuator_outputs = None
+        sensor_combined = None
+        actuator_controls_0 = None
+
+        # Exception handling is done on each dataset separately to find
+        # source of the missing stream.
+
+        # Exception: manual_control_setpoint
         try:
             manual_control_setpoint = ulog.get_dataset('manual_control_setpoint').data
+        except (KeyError, IndexError, ValueError) as error:
+            print("Manual control setpoint not found")
+
+        # Exception: vehicle_local_position
+        try:
             vehicle_local_position = ulog.get_dataset('vehicle_local_position').data
+        except (KeyError, IndexError, ValueError) as error:
+            print("Vehicle local position not found")
+
+        # Exception: vehicle_local_position_setpoint
+        try:
             vehicle_local_position_setpoint = ulog.get_dataset('vehicle_local_position_setpoint').data
+        except (KeyError, IndexError, ValueError) as error:
+            print("Vehicle local position setpoint not found")   
+
+        # Exception: vehicle_attitude_setpoint
+        try:
             vehicle_attitude_setpoint = ulog.get_dataset('vehicle_attitude_setpoint').data
+        except (KeyError, IndexError, ValueError) as error:
+            print("Vehicle attitude setpoint not found")                       
+
+        # Exception: vehicle_rates_setpoint
+        try:
             vehicle_rates_setpoint = ulog.get_dataset('vehicle_rates_setpoint').data
+        except (KeyError, IndexError, ValueError) as error:
+            print("Vehicle rates setpoint not found")  
+
+        # Exception: actuator_outputs
+        try:
             actuator_outputs = ulog.get_dataset('actuator_outputs').data
+        except (KeyError, IndexError, ValueError) as error:
+            print("Actuator output not found")                  
+
+        # Exception: sensor_combined
+        try:
             sensor_combined = ulog.get_dataset('sensor_combined').data
+        except (KeyError, IndexError, ValueError) as error:
+            print("Sensor combined not found")      
+
+        # Exception: actuator_controls_0
+        try:
             actuator_controls_0 = ulog.get_dataset('actuator_controls_0').data
         except (KeyError, IndexError, ValueError) as error:
-            pass
-
+            print("Actuator Controls 0 not found")   
 
         # Get the takeoff location. We use the first position with a valid fix,
         # and assume that the vehicle is not in the air already at that point
@@ -161,124 +206,136 @@ class ThreeDHandler(TornadoRequestHandlerBase):
                 .format(utctimestamp.isoformat(), att_qx, att_qy, att_qz, att_qw, rollSpeed, pitchSpeed, yawSpeed)
         attitude_data += ' ]'
 
+        # Optional data stream serialization starts here.
+        # The code checks None condition to decide whether
+        # to serialize or not.
+
         # Attitude setpoint data
         vehicle_rates_setpoint_data = '[ '
-        for i in range(len(vehicle_rates_setpoint['timestamp'])):
-            rollRateSP = vehicle_rates_setpoint['roll'][i]
-            pitchRateSP = vehicle_rates_setpoint['pitch'][i]
-            yawRateSp = vehicle_rates_setpoint['yaw'][i]
-            t = vehicle_rates_setpoint['timestamp'][i] + utc_offset
-            utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
-                tzinfo=datetime.timezone.utc)
-            vehicle_rates_setpoint_data += '["{:}", {:.6f}, {:.6f}, {:.6f}], ' \
-                .format(utctimestamp.isoformat(), rollRateSP, pitchRateSP, yawRateSp)
+        if vehicle_rates_setpoint is not None:
+            for i in range(len(vehicle_rates_setpoint['timestamp'])):
+                rollRateSP = vehicle_rates_setpoint['roll'][i]
+                pitchRateSP = vehicle_rates_setpoint['pitch'][i]
+                yawRateSp = vehicle_rates_setpoint['yaw'][i]
+                t = vehicle_rates_setpoint['timestamp'][i] + utc_offset
+                utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
+                    tzinfo=datetime.timezone.utc)
+                vehicle_rates_setpoint_data += '["{:}", {:.6f}, {:.6f}, {:.6f}], ' \
+                    .format(utctimestamp.isoformat(), rollRateSP, pitchRateSP, yawRateSp)
         vehicle_rates_setpoint_data += ' ]'        
 
         # Sensor combined data. Includes things like raw gyro, raw accelleration.
         sensor_combined_data = '[ '
-        for i in range(len(sensor_combined['timestamp'])):
-            rawRoll = sensor_combined['gyro_rad[0]'][i]
-            rawPitch = sensor_combined['gyro_rad[1]'][i]
-            rawYaw = sensor_combined['gyro_rad[2]'][i]
-            rawXAcc = sensor_combined['accelerometer_m_s2[0]'][i]
-            rawYAcc = sensor_combined['accelerometer_m_s2[1]'][i]
-            rawZAcc = sensor_combined['accelerometer_m_s2[2]'][i]            
-            t = sensor_combined['timestamp'][i] + utc_offset
-            utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
-                tzinfo=datetime.timezone.utc)
-            sensor_combined_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
-                .format(utctimestamp.isoformat(), rawRoll, rawPitch, rawYaw, rawXAcc, rawYAcc, rawZAcc)
+        if sensor_combined is not None:
+            for i in range(len(sensor_combined['timestamp'])):
+                rawRoll = sensor_combined['gyro_rad[0]'][i]
+                rawPitch = sensor_combined['gyro_rad[1]'][i]
+                rawYaw = sensor_combined['gyro_rad[2]'][i]
+                rawXAcc = sensor_combined['accelerometer_m_s2[0]'][i]
+                rawYAcc = sensor_combined['accelerometer_m_s2[1]'][i]
+                rawZAcc = sensor_combined['accelerometer_m_s2[2]'][i]            
+                t = sensor_combined['timestamp'][i] + utc_offset
+                utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
+                    tzinfo=datetime.timezone.utc)
+                sensor_combined_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
+                    .format(utctimestamp.isoformat(), rawRoll, rawPitch, rawYaw, rawXAcc, rawYAcc, rawZAcc)
         sensor_combined_data += ' ]'         
 
         # Attitude setpoint
         vehicle_attitude_setpoint_data = '[ '
-        for i in range(len(vehicle_attitude_setpoint['timestamp'])):
-            rollSP = vehicle_attitude_setpoint['roll_body'][i]
-            pitchSP = vehicle_attitude_setpoint['pitch_body'][i]
-            yawSP = vehicle_attitude_setpoint['yaw_body'][i]
+        if vehicle_attitude_setpoint is not None:
+            for i in range(len(vehicle_attitude_setpoint['timestamp'])):
+                rollSP = vehicle_attitude_setpoint['roll_body'][i]
+                pitchSP = vehicle_attitude_setpoint['pitch_body'][i]
+                yawSP = vehicle_attitude_setpoint['yaw_body'][i]
 
-            t = vehicle_attitude_setpoint['timestamp'][i] + utc_offset
-            utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
-                tzinfo=datetime.timezone.utc)
-            vehicle_attitude_setpoint_data += '["{:}", {:.6f}, {:.6f}, {:.6f}], ' \
-                .format(utctimestamp.isoformat(), rollSP, pitchSP, yawSP)
+                t = vehicle_attitude_setpoint['timestamp'][i] + utc_offset
+                utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
+                    tzinfo=datetime.timezone.utc)
+                vehicle_attitude_setpoint_data += '["{:}", {:.6f}, {:.6f}, {:.6f}], ' \
+                    .format(utctimestamp.isoformat(), rollSP, pitchSP, yawSP)
         vehicle_attitude_setpoint_data += ' ]'    
 
         # Local Position
         vehicle_local_position_data = '[ '
-        for i in range(len(vehicle_local_position['timestamp'])):
-            xPos = vehicle_local_position['x'][i]
-            yPos = vehicle_local_position['y'][i]
-            zPos = vehicle_local_position['z'][i]
-            xVel = vehicle_local_position['vx'][i]
-            yVel = vehicle_local_position['vy'][i]
-            zVel = vehicle_local_position['vz'][i]            
+        if vehicle_local_position is not None:
+            for i in range(len(vehicle_local_position['timestamp'])):
+                xPos = vehicle_local_position['x'][i]
+                yPos = vehicle_local_position['y'][i]
+                zPos = vehicle_local_position['z'][i]
+                xVel = vehicle_local_position['vx'][i]
+                yVel = vehicle_local_position['vy'][i]
+                zVel = vehicle_local_position['vz'][i]            
 
-            t = vehicle_local_position['timestamp'][i] + utc_offset
-            utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
-                tzinfo=datetime.timezone.utc)
-            vehicle_local_position_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
-                .format(utctimestamp.isoformat(), xPos, yPos, zPos, xVel, yVel, zVel)
+                t = vehicle_local_position['timestamp'][i] + utc_offset
+                utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
+                    tzinfo=datetime.timezone.utc)
+                vehicle_local_position_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
+                    .format(utctimestamp.isoformat(), xPos, yPos, zPos, xVel, yVel, zVel)
         vehicle_local_position_data += ' ]'   
 
         # Local Position Setpoint
         vehicle_local_position_setpoint_data = '[ '
-        for i in range(len(vehicle_local_position_setpoint['timestamp'])):
-            xPosSP = vehicle_local_position_setpoint['x'][i]
-            yPosSP = vehicle_local_position_setpoint['y'][i]
-            zPosSP = vehicle_local_position_setpoint['z'][i]
-            xVelSP = vehicle_local_position_setpoint['vx'][i]
-            yVelSP = vehicle_local_position_setpoint['vy'][i]
-            zVelSP = vehicle_local_position_setpoint['vz'][i]
+        if vehicle_local_position_setpoint is not None:
+            for i in range(len(vehicle_local_position_setpoint['timestamp'])):
+                xPosSP = vehicle_local_position_setpoint['x'][i]
+                yPosSP = vehicle_local_position_setpoint['y'][i]
+                zPosSP = vehicle_local_position_setpoint['z'][i]
+                xVelSP = vehicle_local_position_setpoint['vx'][i]
+                yVelSP = vehicle_local_position_setpoint['vy'][i]
+                zVelSP = vehicle_local_position_setpoint['vz'][i]
 
-            t = vehicle_local_position_setpoint['timestamp'][i] + utc_offset
-            utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
-                tzinfo=datetime.timezone.utc)
-            vehicle_local_position_setpoint_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
-                .format(utctimestamp.isoformat(), xPosSP, yPosSP, zPosSP, xVelSP, yVelSP, zVelSP)
+                t = vehicle_local_position_setpoint['timestamp'][i] + utc_offset
+                utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
+                    tzinfo=datetime.timezone.utc)
+                vehicle_local_position_setpoint_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
+                    .format(utctimestamp.isoformat(), xPosSP, yPosSP, zPosSP, xVelSP, yVelSP, zVelSP)
         vehicle_local_position_setpoint_data += ' ]'       
 
         # Actuator Outputs. This can handle airframes up to 8 actuation outputs (i.e. motors).
         # Tons of formatting things ...
-        num_actuator_outputs = 8
-        max_outputs = np.amax(actuator_outputs['noutputs'])
-        if max_outputs < num_actuator_outputs: num_actuator_outputs = max_outputs
 
         actuator_outputs_data = '[ '
-        for i in range(len(actuator_outputs['timestamp'])):
+        if actuator_outputs is not None:
+            num_actuator_outputs = 8
+            max_outputs = np.amax(actuator_outputs['noutputs'])
+            if max_outputs < num_actuator_outputs: num_actuator_outputs = max_outputs
 
-            t = actuator_outputs['timestamp'][i] + utc_offset
-            utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
-                tzinfo=datetime.timezone.utc)            
+            
+            for i in range(len(actuator_outputs['timestamp'])):
 
-            actuatorList = []
-            actuatorList.append(utctimestamp.isoformat())
-            actuatorDictionary={}
-            formatStringLoop = ''
-            for x in range(max_outputs):
-                actuatorDictionary["actuator_outputs_{0}".format(x)]=actuator_outputs['output['+str(x)+']'][i]
-                formatStringLoop += ', {:.6f}'
-                actuatorList.append(actuatorDictionary["actuator_outputs_{0}".format(x)])
-            formatStringLoop += '], '   
-            formatString = '["{:}"' + formatStringLoop
+                t = actuator_outputs['timestamp'][i] + utc_offset
+                utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
+                    tzinfo=datetime.timezone.utc)            
 
-            actuator_outputs_data += formatString.format(*actuatorList)
+                actuatorList = []
+                actuatorList.append(utctimestamp.isoformat())
+                actuatorDictionary={}
+                formatStringLoop = ''
+                for x in range(max_outputs):
+                    actuatorDictionary["actuator_outputs_{0}".format(x)]=actuator_outputs['output['+str(x)+']'][i]
+                    formatStringLoop += ', {:.6f}'
+                    actuatorList.append(actuatorDictionary["actuator_outputs_{0}".format(x)])
+                formatStringLoop += '], '   
+                formatString = '["{:}"' + formatStringLoop
 
+                actuator_outputs_data += formatString.format(*actuatorList)
         actuator_outputs_data += ' ]'          
 
         # Actuator controls
         actuator_controls_0_data = '[ '
-        for i in range(len(actuator_controls_0['timestamp'])):
-            cont0 = actuator_controls_0['control[0]'][i]
-            cont1 = actuator_controls_0['control[1]'][i]
-            cont2 = actuator_controls_0['control[2]'][i]
-            cont3 = actuator_controls_0['control[3]'][i]
+        if actuator_controls_0 is not None:
+            for i in range(len(actuator_controls_0['timestamp'])):
+                cont0 = actuator_controls_0['control[0]'][i]
+                cont1 = actuator_controls_0['control[1]'][i]
+                cont2 = actuator_controls_0['control[2]'][i]
+                cont3 = actuator_controls_0['control[3]'][i]
 
-            t = actuator_controls_0['timestamp'][i] + utc_offset
-            utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
-                tzinfo=datetime.timezone.utc)
-            actuator_controls_0_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
-                .format(utctimestamp.isoformat(), cont0, cont1, cont2, cont3)
+                t = actuator_controls_0['timestamp'][i] + utc_offset
+                utctimestamp = datetime.datetime.utcfromtimestamp(t/1.e6).replace(
+                    tzinfo=datetime.timezone.utc)
+                actuator_controls_0_data += '["{:}", {:.6f}, {:.6f}, {:.6f}, {:.6f}], ' \
+                    .format(utctimestamp.isoformat(), cont0, cont1, cont2, cont3)
         actuator_controls_0_data += ' ]'     
 
         # handle different vehicle types
