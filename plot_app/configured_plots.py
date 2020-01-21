3a69836bbdcbba295549007c88d11dede08c4a2b
Beat Küng <beat-kueng@gmx.net>
2020-01-21 17:09:47 +0100

    actuator output plots: allow for more than 8 outputs
    Some boards have more than 8 outputs.
    The colors will repeat though, which is something that can be improved.

--- plot_app/configured_plots.py
+++ plot_app/configured_plots.py
@@ -616,12 +616,12 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
     data_plot = DataPlot(data, plot_config, 'actuator_outputs',
                          y_start=0, title='Actuator Outputs (Main)', plot_height='small',
                          changed_params=changed_params, x_range=x_range)
-    num_actuator_outputs = 8
+    num_actuator_outputs = 16
     if data_plot.dataset:
         max_outputs = np.amax(data_plot.dataset.data['noutputs'])
         if max_outputs < num_actuator_outputs: num_actuator_outputs = max_outputs
-    data_plot.add_graph(['output['+str(i)+']' for i in
-                         range(num_actuator_outputs)], colors8[0:num_actuator_outputs],
+    data_plot.add_graph(['output['+str(i)+']' for i in range(num_actuator_outputs)],
+                        [colors8[i % 8] for i in range(num_actuator_outputs)],
                         ['Output '+str(i) for i in range(num_actuator_outputs)], mark_nan=True)
     plot_flight_modes_background(data_plot, flight_mode_changes, vtol_states)
 
@@ -632,7 +632,7 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
                          y_start=0, title='Actuator Outputs (AUX)', plot_height='small',
                          changed_params=changed_params, topic_instance=1,
                          x_range=x_range)
-    num_actuator_outputs = 8
+    num_actuator_outputs = 16
     # only plot if at least one of the outputs is not constant
     all_constant = True
     if data_plot.dataset:
@@ -644,8 +644,8 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
             if not np.all(output_data == output_data[0]):
                 all_constant = False
     if not all_constant:
-        data_plot.add_graph(['output['+str(i)+']' for i in
-                             range(num_actuator_outputs)], colors8[0:num_actuator_outputs],
+        data_plot.add_graph(['output['+str(i)+']' for i in range(num_actuator_outputs)],
+                            [colors8[i % 8] for i in range(num_actuator_outputs)],
                             ['Output '+str(i) for i in range(num_actuator_outputs)], mark_nan=True)
         plot_flight_modes_background(data_plot, flight_mode_changes, vtol_states)
 
