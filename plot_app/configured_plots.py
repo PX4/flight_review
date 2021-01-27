038137bec233fa6a448fb2856a58d2fd851e32d6
Ricardo Marques <marques.ricardo17@gmail.com>
2021-01-27 17:10:09 +0100

    Add plot for third instance of actuator_outputs.
    Signed-off-by: Ricardo Marques <marques.ricardo17@gmail.com>

--- plot_app/configured_plots.py
+++ plot_app/configured_plots.py
@@ -476,20 +476,53 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
                          y_start=0, title='Actuator Outputs (Main)', plot_height='small',
                          changed_params=changed_params, x_range=x_range)
     num_actuator_outputs = 16
+    # only plot if at least one of the outputs is not constant
+    all_constant = True
     if data_plot.dataset:
         max_outputs = np.amax(data_plot.dataset.data['noutputs'])
         if max_outputs < num_actuator_outputs: num_actuator_outputs = max_outputs
-    data_plot.add_graph(['output['+str(i)+']' for i in range(num_actuator_outputs)],
-                        [colors8[i % 8] for i in range(num_actuator_outputs)],
-                        ['Output '+str(i) for i in range(num_actuator_outputs)], mark_nan=True)
-    plot_flight_modes_background(data_plot, flight_mode_changes, vtol_states)
 
-    if data_plot.finalize() is not None: plots.append(data_plot)
+    for i in range(num_actuator_outputs):
+            output_data = data_plot.dataset.data['output['+str(i)+']']
+            if not np.all(output_data == output_data[0]):
+                all_constant = False
+    if not all_constant:
+        data_plot.add_graph(['output['+str(i)+']' for i in range(num_actuator_outputs)],
+                            [colors8[i % 8] for i in range(num_actuator_outputs)],
+                            ['Output '+str(i) for i in range(num_actuator_outputs)], mark_nan=True)
+        plot_flight_modes_background(data_plot, flight_mode_changes, vtol_states)
+
+        if data_plot.finalize() is not None: plots.append(data_plot)
 
     # actuator outputs 1: AUX
     data_plot = DataPlot(data, plot_config, 'actuator_outputs',
                          y_start=0, title='Actuator Outputs (AUX)', plot_height='small',
                          changed_params=changed_params, topic_instance=1,
+                         x_range=x_range)
+    num_actuator_outputs = 16
+    # only plot if at least one of the outputs is not constant
+    all_constant = True
+    if data_plot.dataset:
+        max_outputs = np.amax(data_plot.dataset.data['noutputs'])
+        if max_outputs < num_actuator_outputs: num_actuator_outputs = max_outputs
+
+        for i in range(num_actuator_outputs):
+            output_data = data_plot.dataset.data['output['+str(i)+']']
+            if not np.all(output_data == output_data[0]):
+                all_constant = False
+    if not all_constant:
+        data_plot.add_graph(['output['+str(i)+']' for i in range(num_actuator_outputs)],
+                            [colors8[i % 8] for i in range(num_actuator_outputs)],
+                            ['Output '+str(i) for i in range(num_actuator_outputs)], mark_nan=True)
+        plot_flight_modes_background(data_plot, flight_mode_changes, vtol_states)
+
+        if data_plot.finalize() is not None: plots.append(data_plot)
+
+
+    # actuator outputs 2: EXTRA
+    data_plot = DataPlot(data, plot_config, 'actuator_outputs',
+                         y_start=0, title='Actuator Outputs (EXTRA)', plot_height='small',
+                         changed_params=changed_params, topic_instance=2,
                          x_range=x_range)
     num_actuator_outputs = 16
     # only plot if at least one of the outputs is not constant
