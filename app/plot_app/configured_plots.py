ae0f26fd0aec99d24f46959f3b430bfb1a71cd6d
Beat Küng <beat-kueng@gmx.net>
2021-07-09 10:40:33 +0200

    configured_plots: fix too long line

--- app/plot_app/configured_plots.py
+++ app/plot_app/configured_plots.py
@@ -348,8 +348,9 @@ def generate_plots(ulog, px4_ulog, db_data, vehicle_data, link_to_3d_page,
                              y_axis_label='[ms]', title='Visual Odometry Latency',
                              plot_height='small', changed_params=changed_params,
                              x_range=x_range)
-        data_plot.add_graph([lambda data: ('latency', 1e-3*(data['timestamp'] - data['timestamp_sample']))
-                             ], colors3, ['VIO Latency'], mark_nan=True)
+        data_plot.add_graph(
+            [lambda data: ('latency', 1e-3*(data['timestamp'] - data['timestamp_sample']))],
+            colors3, ['VIO Latency'], mark_nan=True)
         plot_flight_modes_background(data_plot, flight_mode_changes, vtol_states)
 
         if data_plot.finalize() is not None: plots.append(data_plot)
