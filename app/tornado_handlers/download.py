b23da9b9d53a5f83c7ca4a5fac3a009f57950e92
afwilkin <wilkinsaf@gmail.com>
2023-11-01 16:55:36 -0700

    adding QGC compatible parameters

--- app/tornado_handlers/download.py
+++ app/tornado_handlers/download.py
@@ -60,7 +60,6 @@ class DownloadHandler(TornadoRequestHandlerBase):
                 print("DB access failed:", sys.exc_info()[0], sys.exc_info()[1])
             return default_value
 
-
         if download_type == '1': # download the parameters
             ulog = load_ulog_file(log_file_name)
             param_keys = sorted(ulog.initial_parameters.keys())
@@ -68,24 +67,11 @@ class DownloadHandler(TornadoRequestHandlerBase):
             self.set_header("Content-Type", "text/plain")
             self.set_header('Content-Disposition', 'inline; filename=params.txt')
 
-            delimiter = '	'
+            delimiter = ', '
             for param_key in param_keys:
-                self.write("1")
-                self.write(delimiter)
-                self.write("1")
-                self.write(delimiter)
                 self.write(param_key)
                 self.write(delimiter)
                 self.write(str(ulog.initial_parameters[param_key]))
-
-                #if the value is an int write a 6, if not write a 9
-                if(type(ulog.initial_parameters[param_key]) == int):
-                    self.write(delimiter)
-                    self.write("6")
-                else:
-                    self.write(delimiter)
-                    self.write("9")
-
                 self.write('\n')
 
         elif download_type == '2': # download the kml file
@@ -164,6 +150,75 @@ class DownloadHandler(TornadoRequestHandlerBase):
                             self.write(param_key)
                             self.write(delimiter)
                             self.write(str(param_value))
+                            self.write('\n')
+                    except:
+                        pass
+
+        elif download_type == '4': # download QGC parameters
+            ulog = load_ulog_file(log_file_name)
+            param_keys = sorted(ulog.initial_parameters.keys())
+
+            self.set_header("Content-Type", "text/plain")
+            self.set_header('Content-Disposition', 'inline; filename=vehicle.params')
+
+            delimiter = '	'
+            for param_key in param_keys:
+                self.write("1")
+                self.write(delimiter)
+                self.write("1")
+                self.write(delimiter)
+                self.write(param_key)
+                self.write(delimiter)
+                self.write(str(ulog.initial_parameters[param_key]))
+
+                #if the value is an int write a 6, if not write a 9
+                if(type(ulog.initial_parameters[param_key]) == int):
+                    self.write(delimiter)
+                    self.write("6")
+                else:
+                    self.write(delimiter)
+                    self.write("9")
+
+                self.write('\n')
+
+        elif download_type == '5': # download non-default parameters for QGC
+            ulog = load_ulog_file(log_file_name)
+            param_keys = sorted(ulog.initial_parameters.keys())
+
+            self.set_header("Content-Type", "text/plain")
+            self.set_header('Content-Disposition', 'inline; filename=vehicle-nondefault.params')
+            delimiter = '	'
+
+            # Use defaults from log if available
+            if ulog.has_default_parameters:
+                system_defaults = ulog.get_default_parameters(0)
+                airframe_defaults = ulog.get_default_parameters(1)
+                for param_key in param_keys:
+                    try:
+                        param_value = ulog.initial_parameters[param_key]
+                        is_default = True
+                        if param_key in airframe_defaults:
+                            is_default = param_value == airframe_defaults[param_key]
+                        elif param_key in system_defaults:
+                            is_default = param_value == system_defaults[param_key]
+
+                        if not is_default:
+                            self.write("1")
+                            self.write(delimiter)
+                            self.write("1")
+                            self.write(delimiter)
+                            self.write(param_key)
+                            self.write(delimiter)
+                            self.write(str(ulog.initial_parameters[param_key]))
+
+                            #if the value is an int write a 6, if not write a 9
+                            if(type(ulog.initial_parameters[param_key]) == int):
+                                self.write(delimiter)
+                                self.write("6")
+                            else:
+                                self.write(delimiter)
+                                self.write("9")
+
                             self.write('\n')
                     except:
                         pass
