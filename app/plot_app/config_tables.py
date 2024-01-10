f842f811c51de93b1e1f67e45285f7783c02ea8d
Beat Küng <beat-kueng@gmx.net>
2024-01-10 10:36:50 +0100

    flight_modes_table: update list of modes

--- app/plot_app/config_tables.py
+++ app/plot_app/config_tables.py
@@ -6,19 +6,15 @@ flight_modes_table = {
     0: ('Manual', '#cc0000'), # red
     1: ('Altitude', '#eecc00'), # yellow
     2: ('Position', '#00cc33'), # green
+    6: ('Position (Slow)', '#00cc33'), # green
     10: ('Acro', '#66cc00'), # olive
     14: ('Offboard', '#00cccc'), # light blue
     15: ('Stabilized', '#0033cc'), # dark blue
-    16: ('Rattitude', '#ee9900'), # orange
 
     # all AUTO-modes use the same color
     3: ('Mission', '#6600cc'), # purple
     4: ('Loiter', '#6600cc'), # purple
     5: ('Return to Land', '#6600cc'), # purple
-    6: ('RC Recovery', '#6600cc'), # purple
-    7: ('Return to groundstation', '#6600cc'), # purple
-    8: ('Land (engine fail)', '#6600cc'), # purple
-    9: ('Land (GPS fail)', '#6600cc'), # purple
     12: ('Descend', '#6600cc'), # purple
     13: ('Terminate', '#6600cc'), # purple
     17: ('Takeoff', '#6600cc'), # purple
@@ -26,6 +22,7 @@ flight_modes_table = {
     19: ('Follow Target', '#6600cc'), # purple
     20: ('Precision Land', '#6600cc'), # purple
     21: ('Orbit', '#6600cc'), # purple
+    22: ('VTOL Takeoff', '#6600cc'), # purple
     }
 
 vtol_modes_table = {
