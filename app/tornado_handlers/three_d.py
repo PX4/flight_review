PX4/flight_review

commit 13705a16d9b7c17fe129bf2ef93d8138fd943658

3d: update cesium to 1.123
In some cases it was possible to trigger a rendering exception
(when following the model and zooming in closely with small model size).

The separate Bing maps API key is not required anymore.
Also the orientation changed, the 90 deg yaw offset is not required anymore.
And something seems to have changed in the quaternion as well. Not quite
sure what exactly, but now it matches with the previous version.

Cesium changelog:
https://github.com/CesiumGS/cesium/blob/main/CHANGES.md?plain=1

app/plot_app/templates/3d.html
@@ -106,30 +106,25 @@
   <script>
 
 
-{% if bing_api_key != '' %}
-Cesium.BingMapsApi.defaultKey = "{{ bing_api_key }}";
-{% endif %}
-
 {% if cesium_api_key != '' %}
 Cesium.Ion.defaultAccessToken = "{{ cesium_api_key }}";
 {% endif %}
 
+var terrain = Cesium.Terrain.fromWorldTerrain({
+	 requestWaterMask: true,
+	 requestVertexNormals: true
+});
 var viewer = new Cesium.Viewer('cesiumContainer', {
     terrainProviderViewModels : [], //Disable terrain changing
     infoBox : false, //Disable InfoBox widget
     selectionIndicator : false, //Disable selection indicator
-	navigationInstructionsInitiallyVisible : false
+	navigationInstructionsInitiallyVisible : false,
+	terrain : terrain
 });
 var scene = viewer.scene;
 
 //Lighting based on sun/moon positions
 scene.globe.enableLighting = false; // generally a bit too dark when enabled
-
-//Terrain
-viewer.terrainProvider = Cesium.createWorldTerrain({
-    requestWaterMask: true,
-    requestVertexNormals: true
-});
 
 //Enable depth testing so things behind the terrain disappear.
 scene.globe.depthTestAgainstTerrain = true;
@@ -215,22 +210,24 @@ function computeOrientationProperty() {
     var rotation_matrix = new Cesium.Matrix3();
     Cesium.Matrix4.getRotation(transform_matrix, rotation_matrix);
 
-    // rotate by 90 deg to point towards north (instead of east)
-    var m = Cesium.Matrix3.fromRotationZ(Cesium.Math.toRadians(90.0));
-    rotation_matrix = Cesium.Matrix3.multiply(rotation_matrix, m, new Cesium.Matrix3());
     // rotation quaterion from ENU to ECEF
     var q_enu_to_ecef = Cesium.Quaternion.fromRotationMatrix(rotation_matrix);
 
-	
-    
+    var kHalfSqrt2 = 0.7071067811865476;
+    var q_ned_to_enu = new Cesium.Quaternion(kHalfSqrt2, kHalfSqrt2, 0.0, 0.0); // 180 deg along X, -90 deg along Z (intrinsic)
+    var q_ned_to_enu_inv = new Cesium.Quaternion();
+    Cesium.Quaternion.inverse(q_ned_to_enu, q_ned_to_enu_inv);
+    var tmp = new Cesium.Quaternion();
+
     for (i = 0; i < attitude_data.length; ++i) {
         var cur_attitude = attitude_data[i];
         var time_att = Cesium.JulianDate.fromIso8601(cur_attitude[0]);
-        // we need to swap the y & z axis: in NED the body-frame y-axis points to the right
-        // and the z axis down, whereas in ECEF the y-axis points to the left and the z-axis
-        // upwards (x-axis points forward in both coordinate systems)
-        var q = new Cesium.Quaternion(cur_attitude[1], -cur_attitude[2],
-                                               -cur_attitude[3], cur_attitude[4]);
+        var q = new Cesium.Quaternion(cur_attitude[1], cur_attitude[2], cur_attitude[3], cur_attitude[4]);
+
+        // Convert NED to ENU
+        Cesium.Quaternion.multiply(q_ned_to_enu, q, tmp);
+        Cesium.Quaternion.multiply(tmp, q_ned_to_enu_inv, q);
+
         var orientation = new Cesium.Quaternion();
         Cesium.Quaternion.multiply(q_enu_to_ecef, q, orientation);
         //orientationProperty.addSample(time_att, orientation); // this uses interpolation
@@ -331,14 +328,16 @@ var entity = viewer.entities.add({
 
 // sample the ground height at takeoff position to get the offset (there can be
 // an offset of several meters)
-var promise = Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, [ takeoff_position ]);
-Cesium.when(promise, function(updatedPositions) {
-	var ground_offset = takeoff_position.height - takeoff_altitude;
-    console.log('Ground Offset in meters: ' + ground_offset);
-	// re-compute the positions taking the ground offset into account.
-	// add 2 meters more to allow for inaccuracies
-	var positionProperty = computePositionProperty(ground_offset + 2);
-	entity.position = positionProperty;
+terrain.readyEvent.addEventListener(provider => {
+	var promise = Cesium.sampleTerrainMostDetailed(provider, [ takeoff_position ]);
+	promise.then((updatedPositions) => {
+		var ground_offset = takeoff_position.height - takeoff_altitude;
+		console.log('Ground Offset in meters: ' + ground_offset);
+		// re-compute the positions taking the ground offset into account.
+		// add 2 meters more to allow for inaccuracies
+		var positionProperty = computePositionProperty(ground_offset + 2);
+		entity.position = positionProperty;
+	});
 });
 
 

app/tornado_handlers/three_d.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # this is needed for the following imports
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../plot_app'))
-from config import get_bing_maps_api_key, get_cesium_api_key
+from config import get_cesium_api_key
 from helper import validate_log_id, get_log_filename, load_ulog_file, \
     get_flight_mode_changes, flight_modes_table, get_lat_lon_alt_deg
 
@@ -191,6 +191,5 @@ class ThreeDHandler(TornadoRequestHandlerBase):
             model_scale_factor=model_scale_factor,
             model_uri=model_uri,
             log_id=log_id,
-            bing_api_key=get_bing_maps_api_key(),
             cesium_api_key=get_cesium_api_key()))
 

commit a21531f0f0aa79e198f28b1f573b32d1c9264a7f

fix 3d: add 90 deg yaw rotation for plane model
Regression from 13705a16d9b7c17fe129bf2ef93d8138fd943658.
Apparently when I tested it, the previous model was still used due to
caching.

app/plot_app/templates/3d.html
@@ -190,6 +190,7 @@ attitude_data[0][0] = {{ start_timestamp }}; // Prevent initial attitude jump if
 
 var model_scale_factor = {{ model_scale_factor }}; // model-specific scale factor
 var model_uri = "{{ model_uri }}";
+var model_heading_rotation_deg = "{{ model_heading_rotation_deg }}";
 
 //Make sure viewer is at the desired time.
 viewer.clock.startTime = start.clone();
@@ -242,7 +243,7 @@ function computePositionProperty(altitude_offset) {
     return property;
 }
 
-function computeOrientationProperty() {
+function computeOrientationProperty(heading_rotation_deg) {
 	//var orientationProperty = new Cesium.SampledProperty(Cesium.Quaternion);
 	var orientationProperty = new Cesium.TimeIntervalCollectionProperty();
 
@@ -256,6 +257,9 @@ function computeOrientationProperty() {
     var rotation_matrix = new Cesium.Matrix3();
     Cesium.Matrix4.getRotation(transform_matrix, rotation_matrix);
 
+    // Rotate the model to point east
+    var m = Cesium.Matrix3.fromRotationZ(Cesium.Math.toRadians(heading_rotation_deg));
+    rotation_matrix = Cesium.Matrix3.multiply(rotation_matrix, m, new Cesium.Matrix3());
     // rotation quaterion from ENU to ECEF
     var q_enu_to_ecef = Cesium.Quaternion.fromRotationMatrix(rotation_matrix);
 
@@ -299,7 +303,7 @@ function computeOrientationProperty() {
 
 //Compute the entity position & orientation properties
 var positionProperty = computePositionProperty(0);
-var orientationProperty = computeOrientationProperty();
+var orientationProperty = computeOrientationProperty(model_heading_rotation_deg);
 
 // flight modes
 var flightModesProperty = new Cesium.TimeIntervalCollectionProperty();

app/tornado_handlers/three_d.py
@@ -156,9 +156,11 @@ class ThreeDHandler(TornadoRequestHandlerBase):
         # the model_scale_factor should scale the different models to make them
         # equal in size (in proportion)
         mav_type = ulog.initial_parameters.get('MAV_TYPE', None)
+        model_heading_rotation_deg = 0
         if mav_type == 1: # fixed wing
             model_scale_factor = 0.06
             model_uri = 'plot_app/static/cesium/SampleData/models/CesiumAir/Cesium_Air.glb'
+            model_heading_rotation_deg = 90
         elif mav_type == 7: # Airship, controlled
             model_scale_factor = 0.1
             model_uri = 'plot_app/static/cesium/SampleData/models/CesiumBalloon/CesiumBalloon.glb'
@@ -172,6 +174,7 @@ class ThreeDHandler(TornadoRequestHandlerBase):
             # TODO: use the delta-quad model
             model_scale_factor = 0.06
             model_uri = 'plot_app/static/cesium/SampleData/models/CesiumAir/Cesium_Air.glb'
+            model_heading_rotation_deg = 90
         else: # TODO: handle more types
             model_scale_factor = 1
             model_uri = 'plot_app/static/cesium/models/iris/iris.glb'
@@ -190,6 +193,7 @@ class ThreeDHandler(TornadoRequestHandlerBase):
             attitude_data=attitude_data,
             model_scale_factor=model_scale_factor,
             model_uri=model_uri,
+            model_heading_rotation_deg=model_heading_rotation_deg,
             log_id=log_id,
             cesium_api_key=get_cesium_api_key()))
 