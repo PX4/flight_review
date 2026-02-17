plotting: fix overflow in add_virtual_fifo_topic_data
plot_app/plotting.py:69: RuntimeWarning: overflow encountered in scalar add

app/plot_app/plotting.py
@@ -66,7 +66,7 @@ def add_virtual_fifo_topic_data(ulog, topic_name, instance=0):
         scale = cur_dataset.data['scale']
         total_samples = 0
         for i in range(len(t)):
-            total_samples += samples[i]
+            total_samples += int(samples[i])
         t_new = np.zeros(total_samples, t.dtype)
         xyz_new = [np.zeros(total_samples, np.float64) for i in range(3)]
         sample = 0
@@ -77,7 +77,7 @@ def add_virtual_fifo_topic_data(ulog, topic_name, instance=0):
                 for j, axis in enumerate(['x', 'y', 'z']):
                     data_point = cur_dataset.data[axis+'['+str(s)+']'][i] * scale[i]
                     xyz_new[j][sample+s] = data_point
-            sample += samples[i]
+            sample += int(samples[i])
         cur_dataset.data['timestamp'] = t_new
         cur_dataset.data['timestamp_sample'] = t_new
         cur_dataset.data['x'] = xyz_new[0]